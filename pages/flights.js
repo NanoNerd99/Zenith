5e54aa1de55ef8a20611bd08e1410cae6ff03181
NanoNerd99 <mh.karimi350@gmail.com>
2024-05-18 02:55:19 -0400

    fix

--- pages/flights.js
+++ pages/flights.js
@@ -7,24 +7,24 @@ import FlightMiddle from '../src/Components/flightMiddle.js';
 function flights() {
     return (
         <div>
-            {/*<Grid container justifyContent={'center'}>*/}
-            {/*    <FlightMiddle />*/}
-            {/*</Grid>*/}
-            {/*<Grid*/}
-            {/*    container*/}
-            {/*    direction="column"*/}
-            {/*    justifyContent="flex-end"*/}
-            {/*    alignItems="center"*/}
-            {/*    spacing={1}*/}
-            {/*    mt={50}*/}
-            {/*>*/}
-            {/*    <Typography sx={{ fontWeight: '900', fontSize: '22px', mt: 5 }}>Frequently Asked Questions about Flights from New York to Los Angeles</Typography>*/}
-            {/*    {toggleData.map((item,index) => {*/}
-            {/*        return (*/}
-            {/*            <ToggleFlightPage key={index} {...item} />*/}
-            {/*        )*/}
-            {/*    })}*/}
-            {/*</Grid>*/}
+            <Grid container justifyContent={'center'}>
+                <FlightMiddle />
+            </Grid>
+            <Grid
+                container
+                direction="column"
+                justifyContent="flex-end"
+                alignItems="center"
+                spacing={1}
+                mt={50}
+            >
+                <Typography sx={{ fontWeight: '900', fontSize: '22px', mt: 5 }}>Frequently Asked Questions about Flights from New York to Los Angeles</Typography>
+                {toggleData.map((item,index) => {
+                    return (
+                        <ToggleFlightPage key={index} {...item} />
+                    )
+                })}
+            </Grid>
         </div>
     )
 }
